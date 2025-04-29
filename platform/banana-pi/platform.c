651fc5e213a57b31653c781867dff67e720ff7a7
Narendra Varma Dandu <narendandu@gmail.com>
2025-04-29 14:51:37 -0700

    mlo: set common ssid for all vaps (#171)
    Signed-off-by: Narendra Varma <narendandu@gmail.com>

--- platform/banana-pi/platform.c
+++ platform/banana-pi/platform.c
@@ -182,7 +182,7 @@ int platform_get_ssid_default(char *ssid, int vap_index)
             return 0;
         }
     }
-    snprintf(ssid,BPI_LEN_16,"BPI_RDKB-AP%d",vap_index);
+    snprintf(ssid, BPI_LEN_16, "BPI-RDKB-MLO-AP");
     return 0;
 }
 
@@ -216,8 +216,8 @@ int nvram_get_current_password(char *l_password, int vap_index)
 
 int nvram_get_current_ssid(char *l_ssid, int vap_index)
 {
-    wifi_hal_dbg_print("%s:%d \n",__func__,__LINE__); 
-    snprintf(l_ssid,BPI_LEN_16,"BPI_RDKB-AP%d",vap_index);
+    wifi_hal_dbg_print("%s:%d \n",__func__,__LINE__);
+    snprintf(l_ssid, BPI_LEN_16, "BPI-RDKB-MLO-AP");
     return 0;
 }
 
